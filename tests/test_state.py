e600381279ef3c828d9a032809502c4d408479c2
jcovaa <joao.vilacova281205@gmail.com>
2026-03-15 13:50:37 +0000

    Added test for game states

--- tests/test_state.py
+++ tests/test_state.py
@@ -9,6 +9,7 @@ class TestGameState(unittest.TestCase):
       self.capacity = 4
       self.state = GameState(self.bottles, self.capacity)
 
+   # Test class initialization and attributes
    def test_init(self):
       self.assertEqual(self.state.bottles, self.bottles)
       self.assertEqual(self.state.capacity, self.capacity)
@@ -27,6 +28,7 @@ class TestGameState(unittest.TestCase):
       expected_str = "GameState(bottles=[[1, 2, 1, 3], [2, 2, 1, 1], [3, 3, 2, 3], [], []], capacity=4)"
       self.assertEqual(str(self.state), expected_str)
 
+   # Test the pour function
    def test_pour_valid(self):
       result = pour(self.state, 0, 3)
       self.assertIsNotNone(result)
@@ -57,6 +59,30 @@ class TestGameState(unittest.TestCase):
       self.assertEqual(new_state.bottles[1], [2, 2])
       self.assertEqual(new_state.bottles[4], [1, 1])
 
+   # Test the game_states function
+   def test_game_states_successors(self):
+      successors = game_states(self.state)
+
+      self.assertEqual(len(successors), 6)
+      self.assertTrue(all(cost == 1 for _, cost in successors))
+
+      actual_bottles = [next_state.bottles for next_state, _ in successors]
+      actual_cost =[cost for _, cost in successors]
+
+      expected_bottles = [
+         [[1, 2, 1], [2, 2, 1, 1], [3, 3, 2, 3], [3], []],
+         [[1, 2, 1], [2, 2, 1, 1], [3, 3, 2, 3], [], [3]],
+         [[1, 2, 1, 3], [2, 2], [3, 3, 2, 3], [1, 1], []],
+         [[1, 2, 1, 3], [2, 2], [3, 3, 2, 3], [], [1, 1]],
+         [[1, 2, 1, 3], [2, 2, 1, 1], [3, 3, 2], [3], []],
+         [[1, 2, 1, 3], [2, 2, 1, 1], [3, 3, 2], [], [3]],
+      ]
+      
+      self.assertEqual(len(successors), 6)
+      self.assertTrue(all(c == 1 for c in actual_cost))
+      self.assertCountEqual(actual_bottles, expected_bottles)
+
+   # Test the goal state function
    def test_is_goal_state(self):
       goal = GameState([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [], []], 4)
       self.assertTrue(goal_state(goal))
