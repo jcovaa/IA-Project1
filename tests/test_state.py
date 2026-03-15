0fdcda6fc2f9c1e8b91ddbade0c241707a21bb1d
jcovaa <joao.vilacova281205@gmail.com>
2026-03-15 13:35:46 +0000

    Added tests for the pour function

--- tests/test_state.py
+++ tests/test_state.py
@@ -27,6 +27,36 @@ class TestGameState(unittest.TestCase):
       expected_str = "GameState(bottles=[[1, 2, 1, 3], [2, 2, 1, 1], [3, 3, 2, 3], [], []], capacity=4)"
       self.assertEqual(str(self.state), expected_str)
 
+   def test_pour_valid(self):
+      result = pour(self.state, 0, 3)
+      self.assertIsNotNone(result)
+      new_state, cost = result
+      self.assertEqual(cost, 1)
+      self.assertEqual(new_state.bottles[0], [1, 2, 1])
+      self.assertEqual(new_state.bottles[3], [3])
+
+   def test_pour_invalid_same_bottle(self):
+      self.assertIsNone(pour(self.state, 0, 0))
+      
+   def test_pour_source_empty(self):
+      self.assertIsNone(pour(self.state, 3, 0))
+
+   def test_pour_source_complete(self):
+      state1 = GameState([[1, 1, 1, 1], [2, 2, 1, 1], [3, 3, 2, 3], [], []], 4)
+      self.assertIsNone(pour(state1, 0, 3))
+
+   def test_pour_color_mismatch(self):
+      state1 = GameState([[1, 2, 1], [2, 2, 1, 1], [3, 3, 2, 3], [3], []], 4)
+      self.assertIsNone(pour(state1, 1, 3))
+
+   def test_pour_multiple_color(self):
+      result = pour(self.state, 1, 4)
+      self.assertIsNotNone(result)
+      new_state, cost = result
+      self.assertEqual(cost, 1)
+      self.assertEqual(new_state.bottles[1], [2, 2])
+      self.assertEqual(new_state.bottles[4], [1, 1])
+
    def test_is_goal_state(self):
       goal = GameState([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [], []], 4)
       self.assertTrue(goal_state(goal))
