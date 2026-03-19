782bddfbbc7b93dd0c0385bd91f8e250c856de8a
jcovaa <joao.vilacova281205@gmail.com>
2026-03-19 11:55:06 +0000

    Changed main example

--- src/main.py
+++ src/main.py
@@ -14,37 +14,37 @@ def print_solution(node):
 def main():
 	# Example starting bottles (each list: bottom -> top)
 	bottles = [
-		[1, 2, 1],
-		[2, 2, 1],
-		[3, 3, 2],
+		[1, 2, 1, 3],
+		[2, 2, 1, 1],
+		[3, 3, 2, 3],
 		[],
 		[]
 	]
-	capacity = 3
+	capacity = 4
 
 	s = GameState(bottles, capacity)
 	print("Initial state:")
 	print(s)
 
 	print()
-
 	goal_bfs = breadth_first_search(s, goal_state, game_states)
 	print("BFS solution")
-	print_solution(goal_bfs)
+	print(goal_bfs.state)
+
       
-	goal_dfs = depth_first_search(s, goal_state, game_states)
-	print("DFS solution")
-	print_solution(goal_dfs)
+#    goal_dfs = depth_first_search(s, goal_state, game_states)
+#    print("DFS solution")
+#    print_solution(goal_dfs)
 
-	goal_dls = depth_limited_search(s, goal_state, game_states, depth_limit=10)
-	print("DLS solution")
-	print_solution(goal_dls)
+#    goal_dls = depth_limited_search(s, goal_state, game_states, depth_limit=10)
+#    print("DLS solution")
+#    print_solution(goal_dls)
 
-	goal_ids = iterative_deepening_search(s, goal_state, game_states, depth_limit=10)
-	print("IDS solution")
-	print_solution(goal_ids)
+#    goal_ids = iterative_deepening_search(s, goal_state, game_states, depth_limit=10)
+#    print("IDS solution")
+#    print_solution(goal_ids)
 
 
 if __name__ == "__main__":
-	main()
+    main()
 
